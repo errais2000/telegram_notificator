59c24ad348392c452c8aa82746775dab5d1b9082
Cryptoroid <errais2000@gmail.com>
2023-04-10 03:32:41 +0200

    A tiny class to send telegram notifications or alerts from your python programs.

---
+++ telegram_notificator.py
@@ -0,0 +1,15 @@
+import requests
+
+class telegram_notificator:
+  def __init__(self, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID):
+    self.TELEGRAM_BOT_TOKEN = TELEGRAM_BOT_TOKEN
+    self.TELEGRAM_CHAT_ID = TELEGRAM_CHAT_ID
+
+  def send_message(self, message):
+    data = {
+              'chat_id': self.TELEGRAM_CHAT_ID,
+              'text': message,
+              'parse_mode': 'HTML'
+          }
+    return requests.post("https://api.telegram.org/bot{token}/sendMessage".format(token=self.TELEGRAM_BOT_TOKEN), data=data).content
+  
