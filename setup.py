350ff7b399c55e0b73b77928c2147227b34ccad0
Cryptoroid <errais2000@gmail.com>
2023-04-16 23:59:41 +0200

    added home url

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 # Setting up
 setup(
     name="TelegramNotificator",
-    version='1.0.4',
+    version='1.0.5',
     author="Cryptoroid",
     author_email="",
     license='MIT',
     description='A basic telegram notifications sender.',
     long_description_content_type="text/markdown",
     long_description='Send telegram notifications or alerts from your python programs.',
+    url='https://github.com/errais2000/telegram_notificator',
     packages=find_packages(),
     install_requires=['requests'],
     keywords=['telegram', 'message', 'notification', 'alert', 'send message', 'telegram message', 'telegram bot'],
